d66870d69e8caa84439fcd945549f7ba5fbdcb4e
Marius Grondahl <marius@grondahldesign.no>
2019-06-11 10:10:10 +0200

    Started coding

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,32 @@
 // Rover Object Goes Here
 // ======================
-
+var rover = {
+  direction: 'N';
+}
 // ======================
 function turnLeft(rover){
+  rover.direction = 'W';
   console.log("turnLeft was called!");
 }
 
 function turnRight(rover){
+  rover.direction = 'W';
   console.log("turnRight was called!");
 }
 
 function moveForward(rover){
+  rover.direction = 'W';
   console.log("moveForward was called")
+}
+
+
+switch (direction) {
+  case x:
+    // code
+    break;
+  case y:
+    // code
+    break;
+  default:
+    // code
 }
